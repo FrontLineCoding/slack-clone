c9fb4885f5662f1c63b34388cb45bf3227b9c4e0
Andrew Parks <andrew.parks@tutanota.com>
2022-12-07 21:48:39 -0600

    Maybe

--- app/models/workspace.py
+++ app/models/workspace.py
@@ -34,6 +34,9 @@ class Workspace(db.Model):
 
 class WorkspaceMember(db.Model):
     __tablename__ = 'workspace_members'
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
 
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("users.id")), nullable=True)
