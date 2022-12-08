ed6ed7b9d23bfbb16fcd8ab21b96d332581208b1
Andrew Parks <andrew.parks@tutanota.com>
2022-12-07 21:42:57 -0600

    Maybe

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -28,7 +28,7 @@ def seed():
         db.session.execute(f"TRUNCATE table {SCHEMA}.channels RESTART IDENTITY CASCADE;")
         db.session.execute(f"TRUNCATE table {SCHEMA}.messages RESTART IDENTITY CASCADE;")
         db.session.execute(f"TRUNCATE table {SCHEMA}.comments RESTART IDENTITY CASCADE;")
-        db.session.execute(f"TRUNCATE table {SCHEMA}.user_workspaces RESTART IDENTITY CASCADE;")
+        db.session.execute(f"TRUNCATE table {SCHEMA}.workspace_members RESTART IDENTITY CASCADE;")
 
 
         # Add a truncate command here for every table that will be seeded.
